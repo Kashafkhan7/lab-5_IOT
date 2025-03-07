845beee196ad53ba0464c97fd39c03eda8ba7df5
Kashafkhan7 <158587531+Kashafkhan7@users.noreply.github.com>
2025-03-07 14:58:08 +0500

    blynktextrgb.py

--- 3-blynktextrgb.py
+++ 3-blynktextrgb.py
@@ -9,9 +9,10 @@ from neopixel import NeoPixel
 #from machine import Pin, I2C, Timer
 import ssd1306
 
-WIFI_SSID = 'your network/hotspot ssid'
-WIFI_PASS = 'your network/hotspot password'
-BLYNK_AUTH = "your blynk device authentication"
+
+WIFI_SSID = 'NTU FSD'
+WIFI_PASS = ''
+BLYNK_AUTH = "ycn_Gvj2fEkh1OCWF9IY-yEGPK8YJrAN5"
 
 print("Connecting to WiFi network '{}'".format(WIFI_SSID))
 wifi = network.WLAN(network.STA_IF)
